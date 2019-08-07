fe831d39a9cd2d7902232496419b14e326d70f12
Matt Erickson <matt.erickson@auth0.com>
2019-08-07 12:06:08 -0500

    Modify some of the JS

--- js/main.js
+++ js/main.js
@@ -6,13 +6,14 @@ sitemap:
 
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
-    $('.panel-cover').toggleClass('panel-cover--collapsed')
+    $('.panel-cover').toggleClass('panel-cover--collapsed slideOutLeft')
     $('.content-wrapper').toggleClass('animated slideInRight')
   });
 
   if ((window.location.hash && window.location.hash !== undefined) 
   || (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html')){
-    $('.panel-cover').addClass('panel-cover--collapsed')
+    $('.panel-cover').toggleClass('panel-cover--collapsed slideOutLeft')
+    $('.content-wrapper').toggleClass('animated slideInRight')
   }
 
   $('.btn-mobile-menu').click(function () {
@@ -23,23 +24,5 @@ $(document).ready(function () {
   $('.navigation-wrapper .blog-button').click(function () {
     $('.navigation-wrapper').toggleClass('visible')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
-  });
-
-  $('#upcoming .post').each((index, post) => { 
-    const postDate = moment($(post)[0].id, moment.ISO_8601);
-    const nowDate = moment();
-    const upcoming = postDate >= nowDate;
-    if (upcoming) {
-      $(post).removeClass('hidden');
-    }
-  });
-
-  $('#past .post').each((index, post) => { 
-    const postDate = moment($(post)[0].id, moment.ISO_8601);
-    const nowDate = moment();
-    const past = postDate < nowDate;
-    if (past) {
-      $(post).removeClass('hidden');
-    }
   });
 });
